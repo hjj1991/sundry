67dceeb75ae1688868e79f0ec5d87ed04eb2419d
hwangjaejeong <hjj19911@naver.com>
2024-07-28 00:45:02 +0900

    엣지런타임추가

--- app/api/posts/[...slug]/route.ts
+++ app/api/posts/[...slug]/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+export const runtime = 'edge';  // 이 줄을 추가하세요
 import { getPostData } from '@/lib/posts';
 import {NextRequest, NextResponse} from "next/server";
 
