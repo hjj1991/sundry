e0cb34eebc98f897c3ae29c586b07c37dc50c264
황재정 <hjj1991@lezhin.com>
2025-08-06 15:14:18 +0900

    **Update HomeFinancials sorting logic and package dependencies**
    - Adjusted sorting logic in `HomeFinancials.tsx` to reflect API changes.
    - Upgraded multiple npm dependencies, including @babel, @eslint, @mdx-js, and others to newer versions.

--- components/financial/HomeFinancials.tsx
+++ components/financial/HomeFinancials.tsx
@@ -15,7 +15,7 @@ const getFinancials = async (
     params.set("size", "10");
     params.set("financialProductType", financialProductType);
     params.set("depositPeriodMonths", "12");
-    params.set("sort", "maximumInterestRate,desc");
+    params.set("sort", "option.maximumInterestRate,desc");
     const response = await fetch(
         `${process.env.API_SERVER_HOST}/financial-products?${params.toString()}`,
         {
